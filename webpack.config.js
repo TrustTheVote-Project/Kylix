b2982945ae5399216cf70bc71eddbd1f77c36d21
Grace Carey <gracecareymail@gmail.com>
2020-08-23 22:54:32 -0400

    Webpack (#7)
    * Use StepList for address, download, fill out and print steps
    
    * Add react state and component management, and change Address component to Info
    
    * Add webpack loaders (sass, fonts, pdf), example pdf, and start command
    
    * Add download link to example pdf at Download step
    
    * Remove gulp and associated packages (now using webpack exclusively)

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,20 @@
  * Use Webpack + Babel to bundle and transpile our JSX
  */
 const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./index.html",
+  filename: "./index.html",
+});
+
 const config = {
   entry: "./src/scripts/index.js",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist/scripts"),
@@ -20,8 +32,44 @@ const config = {
           },
         ],
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(pdf)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "pdf/",
+            },
+          },
+        ],
+      },
     ],
   },
+  plugins: [htmlPlugin],
 };
 
 module.exports = config;
