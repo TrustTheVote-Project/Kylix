e2d0f4f6641c212fcb82a6dae48831bcfa39d015
GC <gracecareymail@gmail.com>
2021-01-31 20:32:29 -0500

    Fix CORS error

--- src/api/mock-server/app.js
+++ src/api/mock-server/app.js
@@ -7,7 +7,13 @@ const useApiDocs = require('./useApiDocs');
 
 const port = 3000;
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: 'http://localhost:9000', // TODO: move client origin into config
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 const startApp = async () => {
   app.use(bodyParser.json());
