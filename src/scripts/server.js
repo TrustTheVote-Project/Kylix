c8ef4542709fc3fd1c6e46b7977cf776d1d6a2c1
GC <gracecareymail@gmail.com>
2021-03-28 19:20:25 -0400

    Fix path to script

--- src/scripts/server.js
+++ src/scripts/server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use('/', express.static(path.join(__dirname, '../../dist')));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(path.join(__dirname, '../dist', 'index.html')));
+  res.sendFile(path.join(path.join(__dirname, '../../dist', 'index.html')));
 });
 
 const port = process.env.PORT || 3000;
