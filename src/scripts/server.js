e731b93f92c5f12a9095750660ba6856b754b8e4
GC <gracecareymail@gmail.com>
2021-03-28 18:54:21 -0400

    Add prod server script & update better command names

---
+++ src/scripts/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(path.join(__dirname, '../dist', 'index.html')));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port);
