3fb06796d947dec5c0b5e4c454b888ba37555870
GC <gracecareymail@gmail.com>
2021-04-11 21:44:07 -0400

    Remove cors config now that all services are on same domain

--- src/api/mock-server/api-server.js
+++ src/api/mock-server/api-server.js
@@ -1,6 +1,5 @@
 /* eslint no-console: 0 */
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const useRoutes = require('./useRoutes');
 const useApiDocs = require('./useApiDocs');
@@ -8,13 +7,6 @@ const useApiDocs = require('./useApiDocs');
 const apiServer = async (_app, _port) => {
   const app = _app || express();
   const port = _port || process.env.PORT || 5000;
-
-  // const corsOptions = {
-  //   origin: 'http://localhost:9000', // TODO: move client origin into config
-  //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  // }
-
-  // app.use(cors(corsOptions))
 
   app.use(bodyParser.json());
   useRoutes(app);
