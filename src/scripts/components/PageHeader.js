c17c6b59eae61c6e41eaf90de6b583ab9ba2c737
GC <gracecareymail@gmail.com>
2021-06-06 18:10:44 -0400

    Separate PageHeader into separate component

---
+++ src/scripts/components/PageHeader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PageHeader() {
+  return (
+    <header className="ds-base--inverse ds-u-padding--3 ds-u-display--flex ds-u-justify-content--between ds-u-align-items--center">
+      <h1 className="ds-h3 ds-u-margin-bottom--0">Kylix</h1>
+    </header>
+  );
+}
--- src/scripts/containers/DynamicFormFlow.js
+++ src/scripts/containers/DynamicFormFlow.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import DynamicForm from '../components/DynamicForm';
+import PageHeader from '../components/PageHeader';
 
 export default function DynamicFormFlow() {
   return (
     <div className="ds-base">
-      <header className="ds-base--inverse ds-u-padding--3 ds-u-display--flex ds-u-justify-content--between ds-u-align-items--center">
-        <h1 className="ds-h3 ds-u-margin-bottom--0">Kylix</h1>
-      </header>
+      <PageHeader />
       <div className="kx-wrap ds-u-padding-top--3">
         <DynamicForm />
       </div>
