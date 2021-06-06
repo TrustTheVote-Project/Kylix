bfc58ee8a6dc9647cc91abc71877e2dc4c3b3009
GC <gracecareymail@gmail.com>
2021-06-06 19:05:55 -0400

    Add welcome page

--- src/scripts/containers/WelcomePage.js
+++ src/scripts/containers/WelcomePage.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Route, Link,
+} from 'react-router-dom';
+import Button from '@cmsgov/design-system/dist/components/Button/Button';
 import Page from '../components/Page';
 
 export default function DynamicFormFlow() {
+  // eslint-disable-next-line max-len
   return (
     <Page>
-      <div>Welcome</div>
+      <h1>Accessible Ballot Search</h1>
+      <h2>Find, print, and fill out your ballot</h2>
+      <div>
+        {/* eslint-disable-next-line max-len */}
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut bibendum pulvinar posuere.</p>
+        {/* eslint-disable-next-line max-len */}
+        <p>Duis egestas elit vulputate est aliquet, in tincidunt ipsum suscipit. Vivamus sit amet varius justo, vel molestie diam. Etiam dignissim hendrerit est, at vehicula ante euismod id.</p>
+        <ul>
+          <li>Quisque ornare porta sollicitudin.</li>
+          <li>Pellentesque varius ligula in mattis malesuada.</li>
+          <li>Praesent quis nibh elit.</li>
+        </ul>
+        {/* eslint-disable-next-line max-len */}
+        <p>Maecenas sodales enim vitae laoreet imperdiet. Ut enim mauris, commodo sed justo eu, porta sagittis sem. Maecenas tempor, nibh a blandit lobortis, nibh elit sagittis odio, vel feugiat felis mauris nec libero. Nullam feugiat in enim tempor sollicitudin.</p>
+      </div>
+      <Button
+        variation="primary"
+        type="button"
+        size="big"
+        href="/dynamic"
+      >
+        Get Started
+      </Button>
     </Page>
   );
 }
